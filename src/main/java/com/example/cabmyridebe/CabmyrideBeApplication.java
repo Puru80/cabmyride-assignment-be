05b81aca86296ffdab7b72e46e6803e9fed22fbd
Puru80 <puru.agar99@gmail.com>
2023-09-03 11:45:04 +0530

    Innitial commit

---
+++ src/main/java/com/example/cabmyridebe/CabmyrideBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cabmyridebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabmyrideBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabmyrideBeApplication.class, args);
+    }
+
+}
